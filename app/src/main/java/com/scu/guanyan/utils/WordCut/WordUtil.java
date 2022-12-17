2534da7aa860c7d3810dd827bc4088bbfc111aaa
2020141461053 <2287177244@qq.com>
2022-12-17 21:44:48 +0800

    python解释器

---
+++ app/src/main/java/com/scu/guanyan/utils/WordCut/WordUtil.java
@@ -0,0 +1,24 @@
+package com.scu.guanyan.utils.WordCut;
+
+import android.app.Activity;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+import com.chaquo.python.PyException;
+
+/**
+ * @author: 浦博威
+ * @create: 2022-06-30 23:17
+ * @description: python解释器，构造函数传入activity
+ **/
+public class WordUtil {
+   public WordUtil(Activity activity){
+       if (! Python.isStarted()) {
+           Python.start(new AndroidPlatform(activity));
+       }
+   }
+   public String cut(String seq){
+       Python py = Python.getInstance();
+       return py.getModule("lcut").callAttr("lcutseq",seq).toString();
+   }
+}
