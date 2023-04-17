51fd0b5c4397ab98afede0f39532468a8598dd1f
2287177244@qq.com <2287177244@qq.com>
2023-04-17 15:12:15 +0800

    feat: 完成分词功能

--- app/src/main/java/com/scu/guanyan/testActivity.java
+++ app/src/main/java/com/scu/guanyan/testActivity.java
@@ -1,10 +1,13 @@
 package com.scu.guanyan;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
@@ -14,16 +17,31 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.scu.guanyan.utils.ORC.OCRUtils;
+import com.scu.guanyan.utils.word.WordUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class testActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private WordUtil wordUtil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         ivInputImage=findViewById(R.id.imageView2);
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                // 在主线程中更新 UI
+            }
+        });
+
     }
     OCRUtils ocrUtils;
     public void setModel(View view){
@@ -72,6 +90,16 @@ public class testActivity extends AppCompatActivity {
 
     }
 
+    public void testWorkUtil(View view){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                wordUtil=new WordUtil();
+                String str = "2022年度四川大学“青马工程”弘毅班（第十五期）拟推荐共青团工作先进个人名单如上，请大家注意查收";
+                List<String> list=wordUtil.cutSpecial(str);
+            }
+        }).start();
+        }
 
 
 }
